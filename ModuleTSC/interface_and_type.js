54703d3d41829cb9b9719b9c311f29370b6c0bd8
Ekyd <Ekyd777@gmail.com>
2024-01-27 12:05:07 +0300

    added jsx

---
+++ ModuleTSC/interface_and_type.js
@@ -0,0 +1,7 @@
+"use strict";
+const printS = (coord) => {
+};
+const some = (point) => {
+    const d = point; // Каст(явное приведение интерфейса родителя к интерфейс наследника)
+};
+const myCanvas = document.getElementById('canvas'); // Явное приведение к типу
