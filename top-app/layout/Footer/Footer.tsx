2ea013384020d8bcff1777a842b5c8847074b570
Ekyd <Ekyd777@gmail.com>
2024-02-03 20:58:07 +0300

    grid css

--- top-app/layout/Footer/Footer.tsx
+++ top-app/layout/Footer/Footer.tsx
@@ -1,9 +1,16 @@
 import {JSX} from "react";
 import {FooterProps} from "@/layout/Footer/Footer.props";
+import cn from 'classnames';
+import styles from './Footer.module.css';
+import {format} from 'date-fns';
 
-
-const Footer = ({...props}:FooterProps):JSX.Element => {
-  return(<div {...props}>Footer</div>)
+const Footer = ({className, ...props}: FooterProps): JSX.Element => {
+    return (
+        <footer className={cn(className, styles.footer)} {...props}>
+            <div>OwlTop 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+            <a href="#" target={'_blank'}>Пользовательское соглашение</a>
+            <a href="#" target={'_blank'}>Политика конфиденциальности</a>
+        </footer>)
 }
 
 export default Footer;
--- top-app/layout/Layout.tsx
+++ top-app/layout/Layout.tsx
@@ -3,25 +3,23 @@ import React, {FunctionComponent, JSX} from "react";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Sidebar from "./Sidebar/Sidebar";
-
+import styles from './Layout.module.css';
 
 const Layout = ({children}: LayoutProps): JSX.Element => {
     return (
-        <>
-            <Header/>
-            <div>
-                <Sidebar/>
-                <div>
+        <div className={styles.wrapper}>
+            <Header className={styles.header}/>
+            <Sidebar className={styles.sidebar}/>
+                <div className={styles.body}>
                     {children}
                 </div>
-            </div>
-            <Footer/>
-        </>
+            <Footer className={styles.footer}/>
+        </div>
     )
 }
 
 export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) => {
-    return (props: T):JSX.Element => {
+    return (props: T): JSX.Element => {
         return (
             <Layout>
                 <Component {...props}/>
